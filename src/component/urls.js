31bb2a25d7604a0baea31013ae6365e726fb6863
maiquelknechtel1 <maiquelknechtel@gmail.com>
2024-07-24 10:20:54 -0300

    Adicionando urls para endpoint.

--- src/component/urls.js
+++ src/component/urls.js
@@ -3,3 +3,4 @@ export const CREATE_EQUIPMENT = LOCAL + "/equipment-create";
 export const CREATE_CLIENT = LOCAL + "/client-create";
 export const DELETE_USER = LOCAL + "/user-delete";
 export const CREATE_USER = LOCAL + "/user-create";
+export const FIND_ALL_USER = LOCAL + "/user-findAll";
