11d4d863e500aad0184efbb046b9fc10d6a87d74
maiquelknechtel1 <maiquelknechtel@gmail.com>
2024-07-23 22:14:20 -0300

    Adicionando url de delete usuário.

--- src/component/urls.js
+++ src/component/urls.js
@@ -1,3 +1,4 @@
 export const LOCAL = "http://localhost:8080";
 export const CREATE_EQUIPMENT = LOCAL + "/equipment-create";
 export const CREATE_CLIENT = LOCAL + "/client-create";
+export const DELETE_USER = LOCAL + "/user-delete";
